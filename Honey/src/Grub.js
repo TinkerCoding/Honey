50bdd1f2e68742ce2c767e6b0a8de701e7d077ce
TinkerCoding <75763994+TinkerCoding@users.noreply.github.com>
2021-04-06 21:07:36 -0500

    Add files via upload

---
+++ Honey/src/Grub.js
@@ -0,0 +1,12 @@
+var Grub = funtion() {
+  
+    this.age = 0
+    this.color = "pink"
+    this.food = "jelly"
+};
+
+Grub.prototype.constructor = Grub;
+
+Grub.prototype.eat = function(){
+    return "I bee eating"
+}
